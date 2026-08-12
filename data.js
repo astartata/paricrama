283544ad74d5a35df0f5dc109cfce11df2d725d6
astartata <astarta.ta@gmail.com>
2026-08-12 11:31:41 -0700

    Первая версия Парикрамы

---
+++ data.js
@@ -0,0 +1,22 @@
+// Демо-слой данных. На следующем шаге заменяется адаптером Firebase.
+// Правила размещения: genderLock заполняется первой подтвержденной бронью;
+// privateRoom=true означает оплату всех beds мест одним участником/группой.
+window.PLACEMENT_RULES={genderLockMode:'first-confirmed',allowPrivateRoom:true,priceMode:'beds-count',sameGenderOnly:true};
+window.PARIKRAMA_DATA={
+ guests:[
+  {name:'Max Mari',spirit:'Махирадха',gender:'Матаджи',city:'Москва',country:'Россия',tariff:'Премиум 2-местный',room:'',advance:'',remainder:'',refusal:true},
+  {name:'Ivanov Ivan',spirit:'Атма д',gender:'Прабху',city:'Москва',country:'Россия',tariff:'Премиум 2-местный',room:'01_SRSK',advance:'24.11.2026',remainder:'',refusal:false},
+  {name:'Verkhotina Tatiana',spirit:'',gender:'Матаджи',city:'Москва',country:'Россия',tariff:'Комфорт+ 2-местный',room:'',advance:'13.11.2026',remainder:'',refusal:false},
+  {name:'Inty Ghers',spirit:'',gender:'Прабху',city:'',country:'',tariff:'Премиум 2-местный',room:'02_SRSK',advance:'',remainder:'',refusal:false},
+  {name:'П,п псеме',spirit:'Параметрам прабху',gender:'Прабху',city:'',country:'',tariff:'',room:'',advance:'',remainder:'',refusal:false}
+ ],
+ rooms:[
+  {hotel:'Шри Рупа Сева Кундж',tariff:'Премиум 2-местный Этаж 1',roomId:'01_SRSK',beds:2,floor:1,g1:'Ivanov Ivan',g2:'',blocked:false},
+  {hotel:'Шри Рупа Сева Кундж',tariff:'Премиум 2-местный Этаж 1',roomId:'02_SRSK',beds:2,floor:1,g1:'Inty Ghers',g2:'',blocked:false},
+  {hotel:'Шри Рупа Сева Кундж',tariff:'Комфорт+ 2-местный',roomId:'03_SRSK',beds:2,floor:1,g1:'',g2:'',blocked:false}
+ ],
+ placementUnits:[{id:'01_SRSK-1',roomId:'01_SRSK',label:'койка 1',free:true},{id:'01_SRSK-2',roomId:'01_SRSK',label:'койка 2',free:true},{id:'02_SRSK-1',roomId:'02_SRSK',label:'койка 1',free:true},{id:'03_RUKM-1',roomId:'03_RUKM',label:'койка 1',free:true}],
+ payments:[{date:'13.11.2026',name:'Verkhotina Tatiana, Verkhotina Olga',amount:32000,currency:'Рубль',receipt:'https://drive.google.com/open?id=1f0HJhMvIzru-b9WRyOnmn6AVS1aOfxaC',comment:'dfghjk'}],
+ children:[{parent:'Verkhotina Tatiana',childName:'Verkhotina Olga',age:12,tariff:'Комфорт+ 2-местный',room:'',paid:'13.11.2026'}],
+ team:[],vip:[],messages:[{date:'13.11.2026',operator:'bhaktischool.bcs@gmail.com',name:'Ivanov Ivan',channel:'Telegram',topic:'Не зарегистрирован ребёнок 5-14'}]
+};
---
+++ rooms-data.js
@@ -0,0 +1,15 @@
+(function(){
+  const rooms=[];
+  const add=(hotel,tariff,prefix,from,to,beds,floor,extra={})=>{for(let n=from;n<=to;n++){const id=String(n).padStart(2,'0')+'_'+prefix;rooms.push({hotel,tariff,roomId:id,beds,floor,g1:'',g2:'',blocked:false,blockedPlaces:[],...extra[id]})}};
+  add('Шри Рупа Сева Кундж','Премиум 2-местный Этаж 1','SRSK',1,20,2,1,{ '03_SRSK':{g1:'Калпатару дд',g2:'Никунджа Вихарини дд'},'07_SRSK':{g1:'Лила Павани дд',g2:'Дочка Лила Павани дд'},'10_SRSK':{g1:'Санкаршан д',g2:'Слуга Санкаршан д'},'11_SRSK':{g1:'Лила Шакти дд ЮАР'},'12_SRSK':{g1:'Радха Тхакурани дд ЮАР'},'13_SRSK':{g1:'Шачи Деви дд ЮАР'},'14_SRSK':{g1:'Варшана Рани дд',g2:'муж Варшаны Рани'},'15_SRSK':{g1:'Сукумари дд'},'16_SRSK':{g1:'Ом Пурнам дд',g2:'Хари Крипа дд'}});
+  add('Шри Рупа Сева Кундж','Премиум 2-местный Этаж 2','SRSK',21,22,2,2,{});
+  add('Шри Рупа Сева Кундж','Премиум 4-местный Этаж 2','SRSK',23,28,4,2,{});
+  add('Шри Рупа Сева Кундж','Премиум 2-местный Этаж 2','SRSK',29,30,2,2,{'30_SRSK':{g1:'Махапрабху Крипа Д',g2:'Вриндарани Дд'}});
+  add('Рукмани Дхам','Комфорт+ 2-местный','Rukm',1,28,2,'',{'01_Rukm':{g1:'Verkhotina Tatiana',g2:'Verkhotina Olga'}});
+  add('Рукмани Дхам','Комфорт+ 3-местный','Rukm',29,29,3,'',{'29_Rukm':{g1:'Ivanov Oleg',g2:'Ivanova Olga',g3:'Ivanov Vasilii'}});
+  add('Рукмани Дхам','Комфорт+ 4-местный','Rukm',30,30,4,'',{});
+  add('Даудада','Бюджет окна улица','Dau_window',1,10,2,'',{'01_Dau_window':{g1:'Акинчана д',g2:'Махалакшми дд'}});
+  const reserve={};for(let n=1;n<=18;n++)reserve[String(n).padStart(2,'0')+'_Dau_nowindow']={blocked:true,blockedPlaces:[1,2],blockedNote:'резерв'};
+  add('Даудада','Бюджет окна коридор','Dau_nowindow',1,18,2,'',reserve);
+  window.PARIKRAMA_DATA.rooms=rooms;
+})();
