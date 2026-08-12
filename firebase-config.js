0229dad4c98ce0921d8aef7defc4daebb63852df
astartata <astarta.ta@gmail.com>
2026-08-12 14:49:34 -0700

    Исправить вход и форму заявки

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js';
 import { getFirestore, collection, addDoc, getDocs, doc, setDoc, updateDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-storage.js';
 import { getAuth, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js';
 
 const firebaseConfig = {
@@ -14,7 +13,6 @@ const firebaseConfig = {
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
-export const storage = getStorage(firebaseApp);
 export const auth = getAuth(firebaseApp);
 
-window.firebaseReady = Promise.resolve({ db, storage, auth, collection, addDoc, getDocs, doc, setDoc, updateDoc, serverTimestamp, ref, uploadBytes, getDownloadURL, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink, onAuthStateChanged, signOut });
+window.firebaseReady = Promise.resolve({ db, auth, collection, addDoc, getDocs, doc, setDoc, updateDoc, serverTimestamp, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink, onAuthStateChanged, signOut });
